fa3a49c012e9fe7a0a98d5d349b916e7a59d3773
Ruoyu Zhong <zhongruoyu@outlook.com>
2024-04-01 20:40:50 +0800

    Reformat

--- windows.ps1
+++ windows.ps1
@@ -1,28 +1,28 @@
 #!/usr/bin/env pwsh
 
-$ErrorActionPreference="Stop"
+$ErrorActionPreference = "Stop"
 
 function Get-CurrentLLVMStableRelease {
   return (
-      (
-        Invoke-RestMethod "https://community.chocolatey.org/api/v2/Packages()?`$filter=(Id eq 'llvm') and (IsPrerelease eq false)"
-      ).properties.Version |
-        Sort-Object -Descending { [version] $_ } |
-        Select-Object -First 1
-    ).split(".")[0]
+    (
+      Invoke-RestMethod "https://community.chocolatey.org/api/v2/Packages()?`$filter=(Id eq 'llvm') and (IsPrerelease eq false)"
+    ).properties.Version |
+    Sort-Object -Descending { [version] $_ } |
+    Select-Object -First 1
+  ).split(".")[0]
 }
 
-function Install-LLVM {
+function Install-LLVMFromChocolatey {
   param(
     [string]$version
   )
 
   $incremented_version = [int]$version + 1
   $version_full = (
-      Invoke-RestMethod "https://community.chocolatey.org/api/v2/Packages()?`$filter=(Id eq 'llvm') and (IsPrerelease eq false) and (Version ge '$version') and (Version lt '$incremented_version')"
-    ).properties.Version |
-      Sort-Object -Descending { [version] $_ } |
-      Select-Object -First 1
+    Invoke-RestMethod "https://community.chocolatey.org/api/v2/Packages()?`$filter=(Id eq 'llvm') and (IsPrerelease eq false) and (Version ge '$version') and (Version lt '$incremented_version')"
+  ).properties.Version |
+  Sort-Object -Descending { [version] $_ } |
+  Select-Object -First 1
   choco install llvm `
     --allow-downgrade `
     --no-progress `
@@ -34,16 +34,16 @@ function Install-LLVM {
   }
 }
 
-function Run-SanityCheck {
+function Test-Sanity {
   param(
     [string]$version
   )
 
   $llvm_version = (
-      clang --version |
-        Select-Object -First 1 |
-        Select-String -Pattern "\d+(\.\d+)*"
-    ).Matches.Value
+    clang --version |
+    Select-Object -First 1 |
+    Select-String -Pattern "\d+(\.\d+)*"
+  ).Matches.Value
   if ($llvm_version.split(".")[0] -ne $version) {
     Write-Error "Expected LLVM major version $version, got $llvm_version"
     exit 1
@@ -54,5 +54,5 @@ $LLVM_VERSION = $env:LLVM_VERSION
 if (!$LLVM_VERSION) {
   $LLVM_VERSION = Get-CurrentLLVMStableRelease
 }
-Install-LLVM -version $LLVM_VERSION
-Run-SanityCheck -version $LLVM_VERSION
+Install-LLVMFromChocolatey -version $LLVM_VERSION
+Test-Sanity -version $LLVM_VERSION
